d900525d280ba6100af1cdf8871f744fd9573595
Alexander Guerrero <aj.guerreroc@gmail.com>
2023-06-24 12:19:47 -0400

    conexion base de datos vercel

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-export const PGDATABASE = process.env.PGDATABASE;
+/*export const PGDATABASE = process.env.PGDATABASE;
 export const PGHOST = process.env.PGHOST;
 export const PGPASSWORD = process.env.PGPASSWORD;
 export const PGPORT = process.env.PGPORT;
-export const PGUSER = process.env.PGUSER;
+export const PGUSER = process.env.PGUSER;*/
--- database/db.js
+++ database/db.js
@@ -1,13 +1,18 @@
 import pg from 'pg';
 const { Pool } = pg;
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const pool = new Pool({
   // Configuración de la conexión a la base de datos
-  user: process.env.PGUSER,
+
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+ /* user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  port: process.env.PGPORT,*/
 });
 
 export { pool };
